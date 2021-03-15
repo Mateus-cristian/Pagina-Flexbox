fa1efaa9db09baf1d6b539a2f6d08cc25d2542af
Mateus Cristian <mateus_cristian@outlook.com>
2021-03-15 12:10:33 -0300

    Create script.js
    Adicionando JS e função de scroll suave

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function initScrollSuave() {
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+
+    function vaiAteASecao(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        const topo = section.offsetTop;
+
+
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth'
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', vaiAteASecao)
+    });
+}
+
+initScrollSuave();
