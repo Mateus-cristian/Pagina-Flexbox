c90243bc246b8c4469f4cf5aa4e527c7437cf9d8
Mateus Cristian <mateus_cristian@outlook.com>
2021-03-15 14:46:32 -0300

    Create modal.js
    Criado o modulo e o abrir e fechar do modal.

---
+++ script/module/modal.js
@@ -0,0 +1,28 @@
+//Funcao que faz o login da Página 
+
+export default function initModal() {
+
+    const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+    const botaoFechar = document.querySelector('[data-modal="fechar"]');
+    const containerModal = document.querySelector('[data-modal="container"]');
+
+    if (botaoAbrir && botaoFechar && containerModal) {
+
+        function toggleModal(event) {
+            event.preventDefault();
+            containerModal.classList.toggle('ativo')
+        }
+
+        function cliqueForaModal(event) {
+            event.preventDefault();
+            if (event.target === this) {
+                toggleModal(event)
+            }
+
+        }
+
+        botaoAbrir.addEventListener('click', toggleModal);
+        botaoFechar.addEventListener('click', toggleModal);
+        containerModal.addEventListener('click', cliqueForaModal);
+    }
+}
