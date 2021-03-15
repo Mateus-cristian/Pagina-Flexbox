566a9fc9ef304f782f44da613a5c523c719ff954
Mateus Cristian <mateus_cristian@outlook.com>
2021-03-15 14:45:54 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,24 +1,5 @@
-function initScrollSuave() {
-
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-
-    function vaiAteASecao(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        const topo = section.offsetTop;
-
-
-        window.scrollTo({
-            top: topo,
-            behavior: 'smooth'
-        });
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', vaiAteASecao)
-    });
-}
+import initScrollSuave from './module/ScrollSuave.js';
+import initModal from './module/modal.js';
 
 initScrollSuave();
+initModal();
